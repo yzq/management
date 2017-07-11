4af1d0df10ab8ab84b541429ba4198bb0bd4616a
yzq <yan_zhiqiang@foxmail.com>
2017-07-12 00:07:56 +0800

    add eapp

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 from flask_wtf import Form
 from wtforms import StringField, SelectField, SubmitField
-from wtforms.validators import Required, Length
-from app.models import Type
+from wtforms.validators import Required, Length, ValidationError
+from app.models import Type, Element, Eapp
 
 
 class EditForm(Form):
@@ -10,9 +10,56 @@ class EditForm(Form):
     username = StringField(u'用户名', validators=[Required(), Length(1, 64)])
     password = StringField(u'密码', validators=[Required(), Length(1, 64)])
     type = SelectField(u'网元类型', coerce=int)
+    usage = SelectField(u'环境')
+    pc_ip = StringField(u'对接PC')
     submit = SubmitField(u'提交')
 
     def __init__(self, *args, **kwargs):
         super(EditForm, self).__init__(*args, **kwargs)
         self.type.choices = [(type.id, type.name)
                              for type in Type.query.order_by(Type.name).all()]
+        self.usage.choices = [('simulation', u'模拟线'), ('real', u'真实线')]
+
+    def validate_ip(self, field):
+        if Element.query.filter_by(ip=field.data).first():
+            raise ValidationError("Ip already in use")
+
+
+class EditeCNSForm(Form):
+    ip = StringField(u'维护 IP', validators=[Required(), Length(1, 32)])
+    username = StringField(u'用户名', validators=[Required(), Length(1, 64)])
+    password = StringField(u'密码', validators=[Required(), Length(1, 64)])
+    usage = SelectField(u'环境')
+    pc_ip = StringField(u'对接PC')
+    submit = SubmitField(u'提交')
+
+    def __init__(self, *args, **kwargs):
+        super(EditeCNSForm, self).__init__(*args, **kwargs)
+        self.usage.choices = [('simulation', u'模拟线'), ('real', u'真实线')]
+
+    def validate_ip(self, field):
+        if Element.query.filter_by(ip=field.data).first():
+            raise ValidationError("Ip already in use")
+
+
+class EditeAPPForm(Form):
+    mdc_ip = StringField('MDC IP', validators=[Required(), Length(1, 32)])
+    mdc_username = StringField(u'MDC 用户名', validators=[Required(), Length(1, 64)])
+    mdc_password = StringField(u'MDC 密码', validators=[Required(), Length(1, 64)])
+    udc_ip = StringField('UDC IP', validators=[Required(), Length(1, 32)])
+    udc_username = StringField(u'UDC 用户名', validators=[Required(), Length(1, 64)])
+    udc_password = StringField(u'UDC 密码', validators=[Required(), Length(1, 64)])
+    root_username = StringField(u'Linux 用户名', validators=[Required(), Length(1, 64)])
+    root_password = StringField(u'Linux 密码', validators=[Required(), Length(1, 64)])
+    # ubp_username = StringField(u'ubp 用户名', validators=[Required(), Length(1, 64)])
+    ubp_password = StringField(u'ubp 密码', validators=[Required(), Length(1, 64)])
+    submit = SubmitField(u'提交')
+
+
+    def validate_mdc_ip(self, field):
+        if Eapp.query.filter_by(mdc_ip=field.data).first():
+            raise ValidationError("MDC IP already in use")
+
+    # def validate_udc_ip(self, field):
+    #     if Eapp.query.filter_by(udc_ip=field.data).first():
+    #         raise ValidationError("UDC IP already in use")
--- app/models.py
+++ app/models.py
@@ -27,10 +27,45 @@ class Element(db.Model):
     ip = db.Column(db.String(32), unique=True)
     username = db.Column(db.String(64))
     password = db.Column(db.String(64))
-    status = db.Column(db.Boolean, default=False)
+    # status = db.Column(db.Boolean, default=False)
     type_id = db.Column(db.Integer, db.ForeignKey('types.id'))
+    usage = db.Column(db.String(32))
+    pc_ip = db.Column(db.String(32))
 
     def __repr__(self):
         return '<Element %r>' % self.name
+
+
+class Ecns(db.Model):
+    __tablename__ = 'ecns'
+    id = db.Column(db.Integer, primary_key=True)
+    ip = db.Column(db.String(32), unique=True)
+    username = db.Column(db.String(64))
+    password = db.Column(db.String(64))
+    usage = db.Column(db.String(32))
+    pc_ip = db.Column(db.String(32))
+
+    def __repr__(self):
+        return '<Ecns %r>' % self.name
+
+
+class Eapp(db.Model):
+    __tablename__ = 'eapp'
+    id = db.Column(db.Integer, primary_key=True)
+    mdc_ip = db.Column(db.String(32), unique=True)
+    mdc_username = db.Column(db.String(64))
+    mdc_password = db.Column(db.String(64))
+    udc_ip = db.Column(db.String(32), unique=True)
+    udc_username = db.Column(db.String(64))
+    udc_password = db.Column(db.String(64))
+    root_username = db.Column(db.String(64))
+    root_password = db.Column(db.String(64))
+    # ubp_username = db.Column(db.String(64))
+    ubp_password = db.Column(db.String(64))
+
+    def __repr__(self):
+        return '<Eapp %r>' % self.name
+
+
 
 
