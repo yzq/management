820c85b8fd4b6280629871aed125b8506d10bb32
yzq <yan_zhiqiang@foxmail.com>
2017-07-13 00:07:52 +0800

    add enb

--- manage.py
+++ manage.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 import os
 from app import create_app, db
-from app.models import Type, Element
-from flask_script import Manager, Shell
+from app.models import Type, Element, Enb, Frequency
+from flask_script import Manager, Shell, Server
 from flask_migrate import Migrate, MigrateCommand
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
@@ -11,9 +11,12 @@ migrate = Migrate(app, db)
 
 
 def make_shell_context():
-    return dict(app=app, db=db, Element=Element, Type=Type)
+    return dict(app=app, db=db, Element=Element, Type=Type, Enb=Enb, Frequency=Frequency)
+
+
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
+manager.add_command('runserver', Server(host='0.0.0.0'))
 
 if __name__ == '__main__':
     manager.run()
